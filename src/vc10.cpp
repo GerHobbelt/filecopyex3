ec42836057bb4e61b4856db03faadf467055fd26
Ger Hobbelt <ger@hobbelt.com>
2023-10-18 01:17:15 +0200

    fix MSVC compiler errors.

--- src/vc10.cpp
+++ src/vc10.cpp
@@ -30,6 +30,8 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <windows.h>
+
 static PVOID WINAPI ReturnSamePointer(PVOID Ptr) {return Ptr;}
 
 static const char * ProcNames[] = {"EncodePointer", "DecodePointer"};
